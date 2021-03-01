7886afc8a87c86e602c0b2d4f6a102278c3134d8
Jose Luis <joseluisblascoortiz@hotmail.com>
2021-03-01 11:08:39 +0100

    product details OK

--- elcubo-final/frontend/src/App.js
+++ elcubo-final/frontend/src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import {BrowserRouter,Link,Route} from 'react-router-dom';
+import HomeScreen from './Screens/HomeScreen';
+import ProductScreen from './Screens/ProductScreen'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const openMenu = () => {
+        document.querySelector(".sidebar").classList.add("open");
+    };
+
+    const closeMenu = () => {
+        document.querySelector(".sidebar").classList.remove("open");
+    }
+
+    return (
+        <BrowserRouter>
+            <div className="grid-container">
+                <header className="header">
+                    <div className="brand">
+                        <button onClick={openMenu}>
+                            &#9776;
+                </button>
+                <Link to="/">ElCubo</Link>
+                    </div>
+                    <div className="header-links">
+                        <a href="cart.html">Cart</a>
+                        <a href="sigin">Sign In</a>
+                    </div>
+                </header>
+                <aside className="sidebar">
+                    <h3>Shopping categories</h3>
+                    <button className="sidebar-close-button" onClick={closeMenu}>x</button>
+                    <ul>
+                        <li>
+                            <a href="index.html">Pants</a>
+                        </li>
+
+                        <li>
+                            <a href="index.html">Shirts</a>
+                        </li>
+                    </ul>
+                </aside>
+                <main className="main">
+                    <div className="content">
+                        <Route path="/product/:id" component={ProductScreen} />
+                        <Route path="/" exact={true} component={HomeScreen} />
+                    </div>
+
+                </main>
+                <footer className="footer">
+                    All rights reserved.
+        </footer>
+            </div>
+        </BrowserRouter>
+    );
 }
 
 export default App;
